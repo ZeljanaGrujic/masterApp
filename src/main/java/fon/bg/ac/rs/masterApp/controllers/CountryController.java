94cd06fae9805ea1f365a9cf3e8e904aee21c6ed
zg-2106 <zg20180045@gmail.com>
2023-08-20 23:55:23 +0200

    Adding CRUD operations for Region and Location and adding tests for those methodes

--- src/main/java/fon/bg/ac/rs/masterApp/controllers/CountryController.java
+++ src/main/java/fon/bg/ac/rs/masterApp/controllers/CountryController.java
@@ -13,70 +13,70 @@ import java.util.List;
 @Controller
 public class CountryController {
 
-//    @Autowired
-//    private CountryServiceImpl countryServiceImpl;
-//
-//    @Autowired
-//    private LocationServiceImpl locationServiceImpl;
-//
+    @Autowired
+    private CountryServiceImpl countryServiceImpl;
+
+    @Autowired
+    private LocationServiceImpl locationServiceImpl;
+
     @GetMapping("/countries")
     public String getCountries(Model model) {
 
-//        List<CountryDto> countries = countryServiceImpl.getCountries();
-//        System.out.println(countries);
-//        model.addAttribute("countries", countries);
+        List<CountryDto> countries = countryServiceImpl.getCountries();
+        System.out.println(countries);
+        model.addAttribute("countries", countries);
 
 
         //ovaj model saljem ka HTML stranici
 //        fillLocations();
         return "Country";
     }
-//
-//    @PostMapping("/countries/addNew")
-//    public String addBew(CountryDto country) {
-//        try {
-//            CountryDto savedCountry = countryServiceImpl.saveCountry(country);
-//            System.out.println(savedCountry.getId());
-//        }catch (Exception e){
-//            System.out.println("Nije moguce dodati ovaj region/drzavu!");
-//            return "CountrySaveError";
-//
-//        }
-//        return "redirect:/countries";
-//    }
-//
-//    @RequestMapping(value = "/countries/findById/", params = {"id"}, method = RequestMethod.GET)
-//    @ResponseBody
-//    public CountryDto findById(@RequestParam("id") Integer id) {
-//        CountryDto country = countryServiceImpl.findById(id);
-//        System.out.println(country);
-//        return countryServiceImpl.findById(id);
-//    }
-//
-//    @RequestMapping(value = "/countries/update", method = {RequestMethod.PUT, RequestMethod.GET})
-//    public String update(CountryDto country) {
-//        CountryDto updatedCountry = countryServiceImpl.saveCountry(country);
-//        System.out.println(updatedCountry.getId());
-//        return "redirect:/countries";
-//    }
-//
-//
-//    @RequestMapping(value = "/countries/deleteById/", params = {"id"}, method = {RequestMethod.DELETE, RequestMethod.GET})
-//    public String deleteById(@RequestParam("id") Integer id) {
-//        try {
-//
-//            if(locationServiceImpl.findByCountryid(id).isEmpty()) {
-//                countryServiceImpl.deleteById(id);
-//            }else{
-//                throw new Exception("Ne mozete izbrisati ovaj region jer postoje lokacije vezane za ovaj region!");
-//            }
-//
-//        }catch (Exception e){
-//            System.out.println("Ne mozete izbrisati ovu drzavu/region!");
-//            return "CountryDeleteErrorUI";
-//        }
-//        return "redirect:/countries";
-//    }
+
+    @PostMapping("/countries/addNew")
+    public String addBew(CountryDto country) {
+        try {
+            CountryDto savedCountry = countryServiceImpl.saveCountry(country);
+            System.out.println(savedCountry.getId());
+        }catch (Exception e){
+            System.out.println("Nije moguce dodati ovaj region/drzavu!");
+            return "CountrySaveError";
+
+        }
+        return "redirect:/countries";
+    }
+
+    @RequestMapping(value = "/countries/findById/", params = {"id"}, method = RequestMethod.GET)
+    @ResponseBody
+    public CountryDto findById(@RequestParam("id") Integer id) {
+        CountryDto country = countryServiceImpl.findById(id);
+        System.out.println(country);
+        return countryServiceImpl.findById(id);
+    }
+
+    @RequestMapping(value = "/countries/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(CountryDto country) {
+        CountryDto updatedCountry = countryServiceImpl.saveCountry(country);
+        System.out.println(updatedCountry.getId());
+        return "redirect:/countries";
+    }
+
+
+    @RequestMapping(value = "/countries/deleteById/", params = {"id"}, method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String deleteById(@RequestParam("id") Integer id) {
+        try {
+
+            if(locationServiceImpl.findByCountryid(id).isEmpty()) {
+                countryServiceImpl.deleteById(id);
+            }else{
+                throw new Exception("Ne mozete izbrisati ovaj region jer postoje lokacije vezane za ovaj region!");
+            }
+
+        }catch (Exception e){
+            System.out.println("Ne mozete izbrisati ovu drzavu/region!");
+            return "CountryDeleteErrorUI";
+        }
+        return "redirect:/countries";
+    }
 //
 ////    private void fillLocations() {
 ////        List<Country> countries = countryServiceImpl.getCountries();
--- src/main/java/fon/bg/ac/rs/masterApp/controllers/LocationController.java
+++ src/main/java/fon/bg/ac/rs/masterApp/controllers/LocationController.java
@@ -14,64 +14,64 @@ import java.util.List;
 @Controller
 public class LocationController {
 
-//    @Autowired
-//    private LocationServiceImpl locationServiceImpl;
-//
-//    @Autowired
-//    private CountryServiceImpl countryServiceImpl;
+    @Autowired
+    private LocationServiceImpl locationServiceImpl;
+
+    @Autowired
+    private CountryServiceImpl countryServiceImpl;
     @GetMapping("/locations")
     public String getLocations(Model model) {
 
-//        List<LocationDto> locations = locationServiceImpl.getLocations();
-//        List<CountryDto> countries=countryServiceImpl.getCountries();
-//        System.out.println(locations);
-//        System.out.println(countries);
-//        if(locations.isEmpty()){
-//            model.addAttribute("locations", null);
-//        }else {
-//            model.addAttribute("locations", locations);
-//        }
-//        model.addAttribute("countries", countries);
+        List<LocationDto> locations = locationServiceImpl.getLocations();
+        List<CountryDto> countries=countryServiceImpl.getCountries();
+        System.out.println(locations);
+        System.out.println(countries);
+        if(locations.isEmpty()){
+            model.addAttribute("locations", null);
+        }else {
+            model.addAttribute("locations", locations);
+        }
+        model.addAttribute("countries", countries);
 
 
         //ovaj model saljem ka HTML stranici
         return "Location";
     }
-//
-//    @PostMapping("/locations/addNew")
-//    public String addBew(LocationDto location) {
-//        LocationDto savedLocation = locationServiceImpl.saveLocation(location);
-//        System.out.println(savedLocation.getId());
-//        return "redirect:/locations";
-//    }
-//
-//    @RequestMapping(value = "/locations/findById/", params = {"id"}, method = RequestMethod.GET)
-//    @ResponseBody
-//    public LocationDto findById(@RequestParam("id") Integer id) {
-//        LocationDto location = locationServiceImpl.findById(id);
-//        System.out.println(location);
-//        return locationServiceImpl.findById(id);
-//    }
-//
-//    @RequestMapping(value = "/locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
-//    public String update(LocationDto location) {
-//        LocationDto updatedLocation = locationServiceImpl.saveLocation(location);
-//        System.out.println(updatedLocation.getId());
-//        return "redirect:/locations";
-//    }
-//
-//
-//    @RequestMapping(value = "/locations/deleteById/", params = {"id"}, method = {RequestMethod.DELETE, RequestMethod.GET})
-//    public String deleteById(@RequestParam("id") Integer id) {
-//
-//        try {
-//            locationServiceImpl.deleteById(id);
-//        }catch (Exception e){
-//            System.out.println("Ne mozete izbrisati podatke za ovu lokaciju");
-//            return "LocationDeleteError";
-//        }
-//        return "redirect:/locations";
-//    }
+
+    @PostMapping("/locations/addNew")
+    public String addBew(LocationDto location) {
+        LocationDto savedLocation = locationServiceImpl.saveLocation(location);
+        System.out.println(savedLocation.getId());
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value = "/locations/findById/", params = {"id"}, method = RequestMethod.GET)
+    @ResponseBody
+    public LocationDto findById(@RequestParam("id") Integer id) {
+        LocationDto location = locationServiceImpl.findById(id);
+        System.out.println(location);
+        return locationServiceImpl.findById(id);
+    }
+
+    @RequestMapping(value = "/locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(LocationDto location) {
+        LocationDto updatedLocation = locationServiceImpl.saveLocation(location);
+        System.out.println(updatedLocation.getId());
+        return "redirect:/locations";
+    }
+
+
+    @RequestMapping(value = "/locations/deleteById/", params = {"id"}, method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String deleteById(@RequestParam("id") Integer id) {
+
+        try {
+            locationServiceImpl.deleteById(id);
+        }catch (Exception e){
+            System.out.println("Ne mozete izbrisati podatke za ovu lokaciju");
+            return "LocationDeleteError";
+        }
+        return "redirect:/locations";
+    }
 
 
 }
